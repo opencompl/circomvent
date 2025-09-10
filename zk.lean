a42473469253af3c32b89af3d6677238ff619087
Siddharth Bhat <siddu.druid@gmail.com>
2025-09-10 11:51:04 +0100

    chore: embed our program, now start proving soundness + completeness

--- zk.lean
+++ zk.lean
@@ -33,7 +33,7 @@ template IsZero() {
 /-# Exercises for the reader
 
 - Switch to `CompPoly.CMvPolynomial`, which has already been imported.
-  The proofs will need to change; They will use the `RingEquiv` between a 
+  The proofs will need to change; They will use the `RingEquiv` between a
   `CompPoly.CMvPolynomial` and a `MvPolynomial`.
   Since `MPolynomial` contains all the reasoning principles,
   our proofs about `CompPoly.CMvPolynomial` will need to use the `RingEquiv` to
@@ -89,6 +89,15 @@ inductive PolyExpr (α : Type) (p : Nat) : Type
 deriving DecidableEq, Inhabited, Repr
 
 
+instance : OfNat (PolyExpr α p) n where
+  ofNat := PolyExpr.const (n : ZMod p)
+
+instance : Add (PolyExpr α p) where
+  add e1 e2 := PolyExpr.add e1 e2
+
+instance : Mul (PolyExpr α p) where
+  mul e1 e2 := PolyExpr.mul e1 e2
+
 /-- Coerce variables in `α` into a `PolyExpr`.
 
 -- "x" = 0 : Fin 1
@@ -103,7 +112,7 @@ namespace PolyExpr
 variable {α : Type} {p : Nat}
 
 /-- The coercion is well-formed, and injects values `v : α` as a `PolyExpr.var`. -/
-@[simp] theorem coe_var (v : α) : (↑ v : PolyExpr α p) = PolyExpr.var v := rfl
+@[simp] theorem coe_var (v : α) : (v : PolyExpr α p) = PolyExpr.var v := rfl
 
 /-- Denotation from syntax to semantics. -/
 noncomputable def toPolynomial : PolyExpr α p → MvPolynomial α (ZMod p)
@@ -113,7 +122,7 @@ noncomputable def toPolynomial : PolyExpr α p → MvPolynomial α (ZMod p)
 | mul e1 e2 => toPolynomial e1 * toPolynomial e2
 
 /-- Evaluate a `PolyExpr` in an environment `env : α → ZMod p`. -/
-noncomputable def aeval (e : PolyExpr α p) 
+noncomputable def aeval (e : PolyExpr α p)
     (env : α → ZMod p) : ZMod p :=
     (e.toPolynomial).aeval env
 
@@ -124,6 +133,46 @@ def eval (e : PolyExpr α p) (env : α → ZMod p) : ZMod p :=
   | PolyExpr.var v => env v
   | PolyExpr.add e1 e2 => e1.eval env + e2.eval env
   | PolyExpr.mul e1 e2 => e1.eval env * e2.eval env
+
+def neg {α : Type} {p : Nat}
+    (e : PolyExpr α p) : PolyExpr α p :=
+  PolyExpr.mul (PolyExpr.const (-1)) e
+
+instance : Neg (PolyExpr α p) where
+  neg e := neg e
+
+@[simp] theorem neg_def {α : Type} {p : Nat}
+    (e : PolyExpr α p) :
+    (neg e) = (- e) := rfl
+
+@[simp]
+theorem eval_neg {α : Type} {p : Nat}
+    (e : PolyExpr α p) :
+    (- e).eval = fun env => -(e.eval env) := by
+  ext a
+  rw [← neg_def]
+  simp only [neg, eval]
+  ring
+
+def sub {α : Type} {p : Nat}
+    (e1 e2 : PolyExpr α p) : PolyExpr α p :=
+  e1 + (-e2)
+
+instance : Sub (PolyExpr α p) where
+  sub e1 e2 := e1 + (-e2)
+
+@[simp] theorem sub_def {α : Type} {p : Nat}
+    (e1 e2 : PolyExpr α p) :
+    (e1 - e2) = e1.sub e2 := by rfl
+
+@[simp]
+theorem eval_sub {α : Type} {p : Nat}
+    (e1 e2 : PolyExpr α p) :
+    (e1 - e2).eval = fun env => (e1.eval env) - (e2.eval env) := by
+  ext a
+  simp [sub, eval]
+  ring
+
 end PolyExpr
 
 /-- ## Syntax of Boolean expressions.
@@ -160,16 +209,58 @@ theorem BoolExpr.eval_ne {α : Type} {p : Nat}
 These are either polynomials,
 or a top-level ternary select `cond ? etrue : efalse`.
 -/
-inductive ToplevelExpr (α: Type) (p : Nat) : Type
-| poly : PolyExpr α p → ToplevelExpr α p
+
+inductive FieldExpr (α : Type) (p : Nat) : Type
+| const (c : ZMod p) -- TODO: do we want to have this be `Z`, and choose `p` later?
+| var (v : α)
+| add (e1 e2 : FieldExpr α p)
+| mul (e1 e2 : FieldExpr α p)
+| mulinv (e : FieldExpr α p) -- multiplicative inverse
+deriving DecidableEq, Inhabited, Repr
+
+instance : OfNat (FieldExpr α p) n where
+  ofNat := FieldExpr.const (n : ZMod p)
+
+instance : Add (FieldExpr α p) where
+  add e1 e2 := FieldExpr.add e1 e2
+
+instance : Mul (FieldExpr α p) where
+  mul e1 e2 := FieldExpr.mul e1 e2
+
+instance : Inv (FieldExpr α p) where
+  inv e := FieldExpr.mulinv e
+
+def FieldExpr.eval
+    (env : α → ZMod p)
+    (e : FieldExpr α p) : ZMod p :=
+  match e with
+  | FieldExpr.const c => c
+  | FieldExpr.var v => env v
+  | FieldExpr.add e1 e2 => e1.eval env + e2.eval env
+  | FieldExpr.mul e1 e2 => e1.eval env * e2.eval env
+  | FieldExpr.mulinv e => (e.eval env)⁻¹
+
+def FieldExpr.div {α : Type} {p : Nat}
+    (e1 e2 : FieldExpr α p) : FieldExpr α p :=
+  FieldExpr.mul e1 (FieldExpr.mulinv e2)
+
+@[simp]
+theorem FieldExpr.eval_div {α : Type} {p : Nat}
+    (e1 e2 : FieldExpr α p) :
+    (FieldExpr.div e1 e2).eval = fun env => (e1.eval env) * (e2.eval env)⁻¹ := by
+  ext a
+  simp [FieldExpr.div, FieldExpr.eval]
+
+inductive WitnessExpr (α: Type) (p : Nat) : Type
+| field : FieldExpr α p → WitnessExpr α p
 | select (cond : BoolExpr α p) (thenBranch elseBranch : PolyExpr α p)
 
-noncomputable def ToplevelExpr.eval
+noncomputable def WitnessExpr.eval
     (env : α → ZMod p)
-    (t : ToplevelExpr α p) : ZMod p :=
+    (t : WitnessExpr α p) : ZMod p :=
   match t with
-  | ToplevelExpr.poly e => e.eval env
-  | ToplevelExpr.select cond thenBranch elseBranch =>
+  | WitnessExpr.field e => e.eval env
+  | WitnessExpr.select cond thenBranch elseBranch =>
       if cond.eval env then thenBranch.eval env else elseBranch.eval env
 
 /-## Statement expressions.
@@ -180,8 +271,8 @@ noncomputable def ToplevelExpr.eval
   which will treat the constraint also as an assignment to the variable `Var`.
 -/
 inductive Stmt (ι ε ω : Type) (p : Nat)
-| assign (v : Var Empty ε ω) (e : ToplevelExpr (Var ι ε ω) p)
-| constraint (lhs? : Option (Var Empty ε ω)) (c : PolyExpr (Var ι ε ω) p) 
+| assign (v : Var Empty ε ω) (e : WitnessExpr (Var ι ε ω) p)
+| constraint (lhs? : Option (Var Empty ε ω)) (c : PolyExpr (Var ι ε ω) p)
 
 noncomputable def Stmt.toConstraint
     (s : Stmt ι ε ω p) : Option (Constraint (Var ι ε ω) p) :=
@@ -217,7 +308,7 @@ noncomputable def Program.toWitness_go
           -- env' := env [var := o]
           let env' := (fun vnew => if vnew = (var.mapInput Empty.elim) then o else env vnew)
           env'
-        | Stmt.constraint lhs? rhs => 
+        | Stmt.constraint lhs? rhs =>
           match lhs? with
           | none => env
           | some lhs =>
@@ -231,7 +322,7 @@ noncomputable def Program.toWitness
     (prog : Program ι ε ω p) :
     (ω → (ZMod p)) :=
   let env' := prog.toWitness_go fun var =>
-    match var with 
+    match var with
     | Var.input i => envIn i
     | _ => 0
   fun o => env' (Var.output o)
@@ -254,6 +345,31 @@ structure Program.WellFormed
         | Var.existential e => envExists e
         | Var.output o => envOut o)) →
       (envOut = program.toWitness envIn)
+
+namespace IsZeroCircuit
+
+infix : 20 " e≠ " => BoolExpr.ne
+notation "in(" i ")" => Var.input i
+notation "out(" o ")" => Var.output o
+notation "inv(" e ")" => Var.existential e
+notation var "<<=" e => Stmt.constraint (some var) e
+notation var "<<-" e => Stmt.assign var e
+notation e1 "===" e2 => Stmt.constraint none (e1 - e2)
+
+variable (p : Nat) (hp : Nat.Prime p)
+def program : Program (Fin 1) (Fin 1) (Fin 1) p :=
+  { stmts := [
+    inv(0) <<- .field (.var in(0))⁻¹,
+    (out(0)) <<= -(.var (in(0))) * (.var (inv(0))) + 1,
+    (((.var (in(0))) * (.var (out(0))))) === 0
+    ]}
+
+theorem program_WellFormed : (program p).WellFormed := by
+  constructor
+  · sorry
+  · sorry
+
+end IsZeroCircuit
 
 -- 1. define computable MvPolynomial from Nethermind.
 -- 2. define 'Program.WellFormed' : witness (x) = y <->
